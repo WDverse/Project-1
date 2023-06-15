f57c212a996af353e44837e4a41d60151f322c30
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-06-15 18:57:41 -0400

    updated script.js to display location hotels and info and formated project documents

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,39 +3,43 @@ var searchButton = document.getElementById("searchButton");
 var city = document.getElementById("cityName");
 var currTemperature = document.getElementById("temperature");
 var weatherDesc = document.getElementById("weatherDesc");
+var weatherCard = document.getElementById("weather-card");
+// weatherCard.style.display = 'none';
 
 
 // Set content for weather elements and buttons
 var lastCity = localStorage.getItem("lastCity");
 var lastTemp = localStorage.getItem("lastTemp");
 var lastCond = localStorage.getItem("lastCond");
-document.getElementById("weatherIcon").src = "https:"+String(localStorage.getItem("weatherIcon"));
+document.getElementById("weatherIcon").src = "https:" + String(localStorage.getItem("weatherIcon"));
 city.textContent = lastCity;
 
 
 // Get the weather for the most recently searched city
-var weatherURL = "http://api.weatherapi.com/v1/current.json?key=8eec726f852f4f3d83e03157231306&q="+lastCity;
+var weatherURL = "http://api.weatherapi.com/v1/current.json?key=8eec726f852f4f3d83e03157231306&q=" + lastCity;
 
 fetch(weatherURL)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    currTemperature.textContent = "Current Temperature: "+data.current.temp_c+" ºC";
-    weatherDesc.textContent = "Current Weather: "+data.current.condition.text;
-    document.getElementById("weatherIcon").src = "https:"+String(data.current.condition.icon);
-    localStorage.setItem("lastTemp", data.current.temp_c);
-    localStorage.setItem("lastCond", data.current.condition.text);
-    localStorage.setItem("weatherIcon", data.current.condition.icon);
-    localStorage.setItem("ll", String(data.location.lat+"%2C"+data.location.lon));
-});
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        currTemperature.textContent = "Current Temperature: " + data.current.temp_c + " ºC";
+        weatherDesc.textContent = "Current Weather: " + data.current.condition.text;
+        document.getElementById("weatherIcon").src = "https:" + String(data.current.condition.icon);
+        localStorage.setItem("lastTemp", data.current.temp_c);
+        localStorage.setItem("lastCond", data.current.condition.text);
+        localStorage.setItem("weatherIcon", data.current.condition.icon);
+        localStorage.setItem("ll", String(data.location.lat + "%2C" + data.location.lon));
+    });
 
 // Collect city name from the text box
 function setCity() {
-    localStorage.setItem("lastCity",document.getElementById("exampleDataList").value);
+    localStorage.setItem("lastCity", document.getElementById("exampleDataList").value);
+    weatherCard.style.display = 'block';
+
 }
 
 
 // Clickable buttons relevant to weather functions
-searchButton.addEventListener("click",setCity);
+searchButton.addEventListener("click", setCity);
 
