2b86022062587927851250a1c4d9003e2c503acd
Shippo704 <matt.taylor704@yahoo.ca>
2023-06-17 22:09:24 -0400

    fixed bug with hotel list display and cleaned up functionless code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var inputEl = document.getElementById("exampleDataList");
 var city = inputEl.value;
-console.log("city name:" + city);
+var apiKey = 'fsq37w8dUN6194EZq28wvpwF7CQ3pCaNe8lPlAzGIEymFyw='
+var hotelEl = document.getElementById("hotels");
+var hotelHeader = document.getElementById("hotel-header");
 
+// Retrives a list of 10 hotels and displays them in a list
 function getHotel(lat) {
-
-    var apiKey = 'fsq37w8dUN6194EZq28wvpwF7CQ3pCaNe8lPlAzGIEymFyw='
-    var hotelEl = document.getElementById("hotels");
 
     var requestUrl = "https://api.foursquare.com/v3/places/search?query=hotel&ll=" + lat;
     fetch(requestUrl, {
@@ -20,6 +20,11 @@ function getHotel(lat) {
             return response.json();
         })
         .then(function (data) {
+            // Resets the hotel list for the next search
+            if (hotelEl.innerHTML != "") {
+                hotelEl.innerHTML = "";
+            }
+            // Create the list of 10 hotels
             for (i = 0; i < 10; i++) {
                 console.log(data.results[i].name);
                 console.log(data.results[i].location.address);
@@ -49,3 +54,24 @@ function getHotel(lat) {
 
 }
 
+// Display the Hotel Header with the search city name
+function displayHotelHeader(city){
+    hotelHeader.classList.add("hotel-header");
+    hotelHeader.textContent = "Check out these hotels in " + city + ":";
+}
+
+// Collect city name from the text box and display into on webpage
+function setCity(event) {
+    event.preventDefault();
+    // Collect city name
+    var city = document.getElementById("exampleDataList").value;
+    // Display weather card
+    getWeather(city);
+    weatherCard.style.display = 'block';
+    // Display hotel header
+    displayHotelHeader(city);
+}
+
+
+//  Search button is clickable
+searchButton.addEventListener("click", setCity);
